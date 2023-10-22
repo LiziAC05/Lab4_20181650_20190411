03de368cb3c1971ceda73ebea90eaa8b10a702d8
Lizbeth Aquisse Castillo <74085459+LiziAC05@users.noreply.github.com>
2023-10-22 13:50:13 -0500

    diseño + enlace de vistas  4/?

---
+++ app/src/main/java/com/example/lab4_iot/FeedbackActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab4_iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
