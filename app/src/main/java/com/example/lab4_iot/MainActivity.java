6edd208021f09609a4dd1c889e1792f607eaa75f
Lizbeth Aquisse Castillo <74085459+LiziAC05@users.noreply.github.com>
2023-10-22 13:04:55 -0500

    diseño + enlace de vistas  3/?

--- app/src/main/java/com/example/lab4_iot/MainActivity.java
+++ app/src/main/java/com/example/lab4_iot/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.lab4_iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.icu.number.IntegerWidth;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button worker = findViewById(R.id.btnTrabajador);
+        worker.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, TrabajadorActivity.class);
+            startActivity(intent);
+        });
+        Button tutor = findViewById(R.id.btnTutor);
+        tutor.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, TutorActivity.class);
+            startActivity(intent);
+        });
     }
 }
